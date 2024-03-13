2f9c4bb757786704ddf6253e4d1ec789706e94ed
ushasree.k <ushasree3211r@gmail.com>
2024-03-13 12:30:27 +0530

    Your commit message here

--- Add_two_num.py
+++ Add_two_num.py
@@ -5,3 +5,29 @@ def add_numbers(num1, num2):
 
 # function call with two values
 add_numbers(13, 5)
+
+
+####
+def update(x):
+     x = 8
+     print("X", x)
+
+a = 10
+update(a)
+print("a", a)
+
+####
+
+def update(lst):
+    print(id(lst))
+
+
+    lst[1]=25
+    print(id(lst))
+    print("x",lst)
+
+
+lst = [10,20,30]
+print(id(lst))
+update(lst)
+print(lst)
